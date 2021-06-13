ebb7c3222796ce9235b2787d305d6b5001c32afc
Tropes-Ot <ot.server1@outlook.com>
2021-06-13 17:08:25 +0100

    First

---
+++ csc224.py
@@ -0,0 +1,28 @@
+'''
+OLORUNFEMI-OJO DANIEL TOMIWA
+190805503
+COMPUTER SCIENCE
+'''
+import sys
+
+def insert(array, new_value, new_value_index):
+    modified_array = array[:new_value_index - 1]
+    modified_array.append(new_value)
+    for i in range(new_value_index, len(array)):
+        modified_array.append(array[i])
+    array = modified_array
+    print(array)
+
+def delete(array, index_to_delete):
+    modified_array = array[:index_to_delete - 1]
+    for i in range(index_to_delete, len(array)):
+        modified_array.append(array[i])
+    array = modified_array
+    print(array)
+
+def main():
+    #insert([1, 2, 4, 5, 3, 6], 9, 3)
+    delete([1, 2, 4, 5, 3, 6], 2)
+
+if __name__ == "__main__":
+    sys.exit(main())
