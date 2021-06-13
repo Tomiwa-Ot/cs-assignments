d70eecc5ad01a8ad620f7510b9462ed9728f12bf
Tropes-Ot <ot.server1@outlook.com>
2021-06-13 17:21:47 +0100

    update

--- csc224.py
+++ csc224.py
@@ -6,7 +6,7 @@ COMPUTER SCIENCE
 import sys
 
 def insert(array, new_value, new_value_index):
-    modified_array = array[:new_value_index - 1]
+    modified_array = array[:new_value_index]
     modified_array.append(new_value)
     for i in range(new_value_index, len(array)):
         modified_array.append(array[i])
@@ -14,14 +14,14 @@ def insert(array, new_value, new_value_index):
     print(array)
 
 def delete(array, index_to_delete):
-    modified_array = array[:index_to_delete - 1]
-    for i in range(index_to_delete, len(array)):
+    modified_array = array[:index_to_delete]
+    for i in range(index_to_delete + 1, len(array)):
         modified_array.append(array[i])
     array = modified_array
     print(array)
 
 def main():
-    #insert([1, 2, 4, 5, 3, 6], 9, 3)
+    insert([1, 2, 4, 5, 3, 6], 9, 3)
     delete([1, 2, 4, 5, 3, 6], 2)
 
 if __name__ == "__main__":
