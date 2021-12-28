d538c96514be4d4087017c2b576e6e8634ac1513
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2021-12-28 22:25:47 +0100

    Create bestfit_and_firstfit.py

---
+++ operating system I/best fit and first fit/bestfit_and_firstfit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+import sys
+
+'''
+Course: CSC 314
+Description: Implementaion of best fit & first fit memory allocation algorithm
+Group: 5
+Members:
+    1. Mumuni Abdullah          180805047
+    2. Olorunfemi-Ojo Tomiwa    190805503
+    3. Animashaun Sofiat        190805520
+    4. Ogunrinde Motunrayo      190805514
+    5. Ademuyiwa Abdulbaqi      190805522
+'''
