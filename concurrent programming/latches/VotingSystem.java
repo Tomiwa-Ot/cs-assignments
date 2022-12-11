42113d14f98f372265288e0510317a9b0c63ba8c
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-12-11 23:31:34 +0100

    added some files

--- concurrent programming/latches/VotingSystem.java
+++ concurrent programming/latches/VotingSystem.java
@@ -1,1 +1,69 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class VotingSystem {
+
+	public static int candidateA = 0, candidateB = 0, candidateC = 0;
+
+	public static void main(String[] args) {
+		CountDownLatch voters = new CountDownLatch(50);
+		ExecutorService service = Executors.newCachedThreadPool();
+		for(int i = 0; i < 50; i++) {
+			service.execute(new Runnable() {
+				@Override
+				public void run() {
+					vote(voters);
+				}
+			});
+		}
+		
+		service.shutdown();
+		try {
+			voters.await();
+			calculateResults();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static synchronized void vote(CountDownLatch voters) {
+		int choice = (int) (Math.random() * 3);
+		switch (choice) {
+			case 0:
+				candidateA++;
+				break;
+			case 1:
+				candidateB++;
+				break;
+			case 2:
+				candidateC++;
+				break;
+			default:
+				break;
+		}
+		voters.countDown();
+	}
+	
+	public static void calculateResults() {
+		ArrayList<Integer> results = new ArrayList<Integer>();
+		Collections.addAll(results, candidateA, candidateB, candidateC);
+		System.out.println("A: " + results.get(0) + ", B: " + results.get(1) + ", C: " + results.get(2));
+		int highestVotes = Collections.max(results);
+		switch (results.indexOf(highestVotes)) {
+			case 0:
+				System.out.println("Candidate A won");
+				break;
+			case 1:
+				System.out.println("Candidate B won");
+				break;
+			case 2:
+				System.out.println("Candidate C won");
+				break;
+			default:
+				break;
+		}
+	}
+}
--- operating system I/multiple_level_queue.py
+++ operating system I/multiple_level_queue.py
@@ -1,1 +1,114 @@
+#!/usr/bin/env python3
 
+from __future__ import print_function
+import copy
+
+__description__ = "Implementation of multiple level queue scheduling algorithm"
+
+jobs = [
+    {"id" : 1, "cpu cycle" : 8, "queue" : 1},
+    {"id" : 2, "cpu cycle" : 4, "queue" : 2},
+    {"id" : 3, "cpu cycle" : 1, "queue" : 2},
+    {"id" : 4, "cpu cycle" : 5, "queue" : 1},
+    {"id" : 5, "cpu cycle" : 2, "queue" : 2},
+    {"id" : 6, "cpu cycle" : 3, "queue" : 1}
+]
+
+# NB: Queue 1 has higher priority than Queue 2
+queue1, queue2 = [], []
+
+
+def no_movement_btw_queues(queue1, queue2):
+    '''Multiple level scheduling with no movement between queues'''
+    priority, execution_sequence = [queue1, queue2], []
+    execution_time = [0]
+    for queue in priority:
+        for job in queue:
+            execution_sequence.append(job["id"])
+            execution_time.append(execution_time[-1] + job["cpu cycle"])
+    print("No movement between queues: ", end="")
+    print(str([f"P{x}" for x in execution_sequence])[1:-1].replace(",", " ->"))
+    print(f"Execution time: {str(execution_time)[1:-1]}\n")
+    
+
+def movement_btw_queues(queue1, queue2):
+    '''Multiple level scheduling with movement between queues'''
+    time_slice = 3
+    priority, execution_sequence = [queue1, queue2], []
+    execution_time = [0]
+    for i, queue in enumerate(priority):
+        for job in queue:
+            execution_sequence.append(job["id"])
+            # Keep track of execution timeline
+            if job["cpu cycle"] - time_slice > 0:
+                execution_time.append(execution_time[-1] + time_slice)
+            else:
+                execution_time.append(execution_time[-1] + job["cpu cycle"])
+            # Check if job has finished executing
+            job["cpu cycle"] -= time_slice
+            if job["cpu cycle"] > 0:
+                # Add job to next queue
+                if i == len(priority) - 1:
+                    priority[-1].append(job)
+                else:
+                    priority[i + 1].append(job)
+            else:   
+                del job
+    print("Movement between queues: ", end="")
+    print(str([f"P{x}" for x in execution_sequence])[1:-1].replace(",", " ->"))
+    print(f"Execution time: {str(execution_time)[1:-1]}\n")
+
+
+def variable_time_quantum_per_queue(queue1, queue2):
+    '''Multiple level scheduling with variable time quantum per queue'''
+    time_slice = 3
+    priority, execution_sequence = [queue1, queue2], []
+    execution_time = [0]
+    for i, queue in enumerate(priority):
+        for job in queue:
+            execution_sequence.append(job["id"])
+            # Keep track of execution timeline
+            if job["cpu cycle"] - time_slice > 0:
+                execution_time.append(execution_time[-1] + time_slice)
+            else:
+                execution_time.append(execution_time[-1] + job["cpu cycle"])
+            # Check if job has finished executing
+            job["cpu cycle"] -= time_slice
+            if job["cpu cycle"] > 0:
+                # Add job to next queue
+                if i == len(priority) - 1:
+                    priority[-1].append(job)
+                else:
+                    priority[i + 1].append(job)
+            else:
+                del job
+        # Double time slice
+        time_slice += time_slice
+    print("Variable time quantum per queue: ", end="")
+    print(str([f"P{x}" for x in execution_sequence])[1:-1].replace(",", " ->"))
+    print(f"Execution time: {str(execution_time)[1:-1]}")
+
+
+
+if __name__ == "__main__":
+    # Place jobs in their respective queues
+    for job in jobs:
+        if job["queue"] == 1:
+            queue1.append(job)
+        else:
+            queue2.append(job)
+    
+    # Display process, cpu cycle and queue
+    print("-"*24)
+    print("| {:<5} {:<10} {:<5}".format("PID", "CPU Cycle", "Queue"))
+    print("-"*24)
+    for job in jobs:
+        print(
+            "| {:<5} {:<10} {:<5}"
+            .format(str(job["id"]), job["cpu cycle"], str(job["queue"]))
+        )
+    print("-"*24)
+    
+    no_movement_btw_queues(copy.deepcopy(queue1), copy.deepcopy(queue2))
+    movement_btw_queues(copy.deepcopy(queue1), copy.deepcopy(queue2))
+    variable_time_quantum_per_queue(copy.deepcopy(queue1), copy.deepcopy(queue2))
