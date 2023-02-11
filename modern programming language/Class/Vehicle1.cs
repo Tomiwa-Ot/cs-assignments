74d5f562bd9f690c95de3e56f685f3ead878aec1
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2023-02-11 08:22:35 +0100

    added some files

---
+++ modern programming language/Class/Vehicle1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Selection
+{
+    class Vehicle
+    {
+        // Fields
+        private string name;
+        private int topSpeed;
+
+        // Constructor
+        public Vehicle(string name, int topSpeed)
+        {
+            this.name = name;
+            this.topSpeed = topSpeed;
+        }
+
+        // Method
+        public double SpeedInMetresPerSecond(int speed)
+        {
+            return speed * (5.0/18.0);
+        }
+
+        // Properties
+        public double MilesPerGallon { get; set; }
+        public double Cylinders { get; set; }
+    }
+}
---
+++ modern programming language/Class/Vehicle2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Selection
+{
+    class Vehicle
+    {
+        // Constructor
+        public Vehicle(string name, int topSpeed)
+        {
+            Console.WriteLine($"{name} has a top speed of {topSpeed}km/h")
+        }
+
+        // Method
+        public double SpeedInMetresPerSecond(int speed)
+        {
+            return speed * (5.0/18.0);
+        }
+
+        // Properties
+        public double MilesPerGallon { get; set; }
+        public double Cylinders { get; set; }
+    }
+}
