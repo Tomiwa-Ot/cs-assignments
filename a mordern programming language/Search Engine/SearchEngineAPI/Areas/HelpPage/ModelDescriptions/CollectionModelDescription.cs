0a0903ecdee0797943dc8ba52d52b98009293674
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2023-05-31 20:07:41 +0100

    made changes

--- a mordern programming language/Search Engine/SearchEngineAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace SearchEngineAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- a mordern programming language/Search Engine/SearchEngineAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SearchEngineAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
